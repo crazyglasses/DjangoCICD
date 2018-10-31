a1b814b816454a9fcde8cfa171dd083c452c0cbc
crazyglasses <anmoluday@gmail.com>
2018-10-31 05:35:03 +0000

    back to base

--- containers/app/blog/views.py
+++ containers/app/blog/views.py
@@ -14,9 +14,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts, 'pod_name': settings.MY_POD_NAME})
 
-def selfdestruct(request):
-    signal.signal(signal.SIGTERM, sigterm_handler)
-
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post, 'pod_name': settings.MY_POD_NAME})
